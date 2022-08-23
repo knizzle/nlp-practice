d3c99f9cfac308cebfde51ed8286582db6753228
kelsey <klsycanoy@yahoo.com>
2022-08-22 18:11:28 -0700

    wk1 assnt in progress

--- tensorflow/keras_intro.py
+++ tensorflow/keras_intro.py
@@ -1,7 +1,7 @@
-from msilib import sequence
 import tensorflow as tf
 from tensorflow import keras 
 from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 sentences = [
     'I love my dog',
@@ -34,3 +34,20 @@ test_seq = tokenizer.texts_to_sequences(test_data)
 # print(test_seq)
 # w/out oov >> [[4, 2, 1, 3], [1, 3, 1]] = 'i love my dog', 'my dog my' 
 # w/oov >> [[5, 1, 3, 2, 4], [2, 4, 1, 2, 1]]
+
+padded = pad_sequences(sequences, padding='post', truncating='post', maxlen=5)
+
+# print(word_index) 
+# {'<OOV': 1, 'my': 2, 'love': 3, 'dog': 4, 'i': 5, 'you': 6, 'cat': 7, 'do': 8, 'think': 9, 'is': 10, 'amazing': 11}
+# print(sequences)
+# [[5, 3, 2, 4], [5, 3, 2, 7], [6, 3, 2, 4, 8, 6, 9, 2, 4, 10, 11]]
+# print(padded) w/out padding='post', truncating='post', and maxlen
+# [[ 0  0  0  0  0  0  0  5  3  2  4]
+#  [ 0  0  0  0  0  0  0  5  3  2  7]
+#  [ 6  3  2  4  8  6  9  2  4 10 11]]
+
+# print(padded)
+# w/'post' and maxlen
+# [[5 3 2 4 0]
+#  [5 3 2 7 0]
+#  [6 3 2 4 8]]
